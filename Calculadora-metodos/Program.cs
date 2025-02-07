ca5c07e3d5a0f1b9a5857764511340403fe0053a
Marcos Vinícius <marcosviniciusdiasalmeida@gmail.com>
2025-02-07 18:34:50 -0300

    adicionando o resto do código

--- Calculadora-metodos/Program.cs
+++ Calculadora-metodos/Program.cs
@@ -16,5 +16,34 @@ class Program
         Console.WriteLine("2. Subtração (-)");
         Console.WriteLine("3. Multiplicação (*)");
         Console.WriteLine("4. Divisão (/)");
-        
+           int operacao = calculadora.LerOperacao();
+
+        switch (operacao)
+        {
+            case 1:
+                Console.WriteLine($"Resultado: {num1} + {num2} = {Calculadora.Adicionar(num1, num2)}");
+                break;
+            case 2:
+                Console.WriteLine($"Resultado: {num1} - {num2} = {Calculadora.Subtrair(num1, num2)}");
+                break;
+            case 3:
+                Console.WriteLine($"Resultado: {num1} * {num2} = {calculadora.Multiplicar(num1, num2)}");
+                break;
+            case 4:
+                if (num2 == 0)
+                {
+                    Console.WriteLine("Erro: Divisão por zero não é permitida.");
+                }
+                else
+                {
+                    Console.WriteLine($"Resultado: {num1} / {num2} = {Calculadora.Dividir(num1, num2)}");
+                }
+                break;
+        }
+
+        Console.WriteLine("Pressione qualquer tecla para sair...");
+        Console.ReadKey();
+    }
+
+}
 
