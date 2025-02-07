aa8eb67cef141ff85c52d16184c26b08d8de503d
Marcos Vinícius <marcosviniciusdiasalmeida@gmail.com>
2025-02-07 18:33:28 -0300

    adicionando as operações

--- Calculadora-metodos/Calculadora.cs
+++ Calculadora-metodos/Calculadora.cs
@@ -2,4 +2,8 @@ using System;
 
 namespace CalculadoraApp
 {
-  
+        public static double Adicionar(double a, double b) => a + b;
+        public static double Subtrair(double a, double b) => a - b;
+        public  double Multiplicar(double a, double b) => a * b;
+
+        public static double? Dividir(double a, double b)
--- Calculadora-metodos/Program.cs
+++ Calculadora-metodos/Program.cs
@@ -4,4 +4,17 @@ using CalculadoraApp;
 class Program
 {
     static void Main(string[] args)
- 
+      {
+       Console.WriteLine("Bem-vindo à Calculadora!");
+        Calculadora calculadora = new Calculadora();
+
+        double num1 = calculadora.LerNumero("Digite o primeiro número: ");
+        double num2 = calculadora.LerNumero("Digite o segundo número: ");
+
+        Console.WriteLine("Escolha a operação:");
+        Console.WriteLine("1. Adição (+)");
+        Console.WriteLine("2. Subtração (-)");
+        Console.WriteLine("3. Multiplicação (*)");
+        Console.WriteLine("4. Divisão (/)");
+        
+
